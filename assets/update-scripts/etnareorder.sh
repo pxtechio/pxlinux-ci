ed642820c94149f7579bf22442a4e4bfc4f4e1a7
Jose Ruiz <jose.ruiz@codeandmodules.com>
2019-04-11 08:43:31 -0700

    Fixed .gitignore

--- assets/update-scripts/etnareorder.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp /update-scripts/etnareorder /bin/etnareorder
-cp /update-scripts/pixieEtnaReorder.service /lib/systemd/system/pixieEtnaReorder.service
-systemctl enable pixieEtnaReorder.service
